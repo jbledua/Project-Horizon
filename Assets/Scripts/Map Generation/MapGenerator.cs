0aad3ff54e734e0a6740ffe3dc325a19a41b14ed
Josiah Ledua <jbledua@backslashdesigns.ca>
2024-11-30 19:23:53 -0500

    Added Collision Mesh to Terrain

--- Assets/Scripts/Map Generation/MapGenerator.cs
+++ Assets/Scripts/Map Generation/MapGenerator.cs
@@ -1,24 +1,23 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System;
 using System.Threading;
 using System.Collections.Generic;
 
-public class MapGenerator : MonoBehaviour
-{
+public class MapGenerator : MonoBehaviour {
 
-	public enum DrawMode { NoiseMap, ColourMap, Mesh, FalloffMap };
+	public enum DrawMode {NoiseMap, ColourMap, Mesh, FalloffMap};
 	public DrawMode drawMode;
 
 	public Noise.NormalizeMode normalizeMode;
 
-	public const int mapChunkSize = 241;
-	[Range(0, 6)]
+	public const int mapChunkSize = 239;
+	[Range(0,6)]
 	public int editorPreviewLOD;
 	public float noiseScale;
 
 	public int octaves;
-	[Range(0, 1)]
+	[Range(0,1)]
 	public float persistance;
 	public float lacunarity;
 
@@ -39,113 +38,85 @@ public class MapGenerator : MonoBehaviour
 	Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
 	Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
 
-	void Awake()
-	{
-		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
+	void Awake() {
+		falloffMap = FalloffGenerator.GenerateFalloffMap (mapChunkSize);
 	}
 
-	public void DrawMapInEditor()
-	{
-		MapData mapData = GenerateMapData(Vector2.zero);
+	public void DrawMapInEditor() {
+		MapData mapData = GenerateMapData (Vector2.zero);
 
-		MapDisplay display = FindObjectOfType<MapDisplay>();
-		if (drawMode == DrawMode.NoiseMap)
-		{
-			display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.heightMap));
-		}
-		else if (drawMode == DrawMode.ColourMap)
-		{
-			display.DrawTexture(TextureGenerator.TextureFromColourMap(mapData.colourMap, mapChunkSize, mapChunkSize));
-		}
-		else if (drawMode == DrawMode.Mesh)
-		{
-			display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColourMap(mapData.colourMap, mapChunkSize, mapChunkSize));
-		}
-		else if (drawMode == DrawMode.FalloffMap)
-		{
+		MapDisplay display = FindObjectOfType<MapDisplay> ();
+		if (drawMode == DrawMode.NoiseMap) {
+			display.DrawTexture (TextureGenerator.TextureFromHeightMap (mapData.heightMap));
+		} else if (drawMode == DrawMode.ColourMap) {
+			display.DrawTexture (TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
+		} else if (drawMode == DrawMode.Mesh) {
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColourMap (mapData.colourMap, mapChunkSize, mapChunkSize));
+		} else if (drawMode == DrawMode.FalloffMap) {
 			display.DrawTexture(TextureGenerator.TextureFromHeightMap(FalloffGenerator.GenerateFalloffMap(mapChunkSize)));
 		}
 	}
 
-	public void RequestMapData(Vector2 centre, Action<MapData> callback)
-	{
+	public void RequestMapData(Vector2 centre, Action<MapData> callback) {
 		ThreadStart threadStart = delegate {
-			MapDataThread(centre, callback);
+			MapDataThread (centre, callback);
 		};
 
-		new Thread(threadStart).Start();
+		new Thread (threadStart).Start ();
 	}
 
-	void MapDataThread(Vector2 centre, Action<MapData> callback)
-	{
-		MapData mapData = GenerateMapData(centre);
-		lock (mapDataThreadInfoQueue)
-		{
-			mapDataThreadInfoQueue.Enqueue(new MapThreadInfo<MapData>(callback, mapData));
+	void MapDataThread(Vector2 centre, Action<MapData> callback) {
+		MapData mapData = GenerateMapData (centre);
+		lock (mapDataThreadInfoQueue) {
+			mapDataThreadInfoQueue.Enqueue (new MapThreadInfo<MapData> (callback, mapData));
 		}
 	}
 
-	public void RequestMeshData(MapData mapData, int lod, Action<MeshData> callback)
-	{
+	public void RequestMeshData(MapData mapData, int lod, Action<MeshData> callback) {
 		ThreadStart threadStart = delegate {
-			MeshDataThread(mapData, lod, callback);
+			MeshDataThread (mapData, lod, callback);
 		};
 
-		new Thread(threadStart).Start();
+		new Thread (threadStart).Start ();
 	}
 
-	void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback)
-	{
-		MeshData meshData = MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
-		lock (meshDataThreadInfoQueue)
-		{
-			meshDataThreadInfoQueue.Enqueue(new MapThreadInfo<MeshData>(callback, meshData));
+	void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback) {
+		MeshData meshData = MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
+		lock (meshDataThreadInfoQueue) {
+			meshDataThreadInfoQueue.Enqueue (new MapThreadInfo<MeshData> (callback, meshData));
 		}
 	}
 
-	void Update()
-	{
-		if (mapDataThreadInfoQueue.Count > 0)
-		{
-			for (int i = 0; i < mapDataThreadInfoQueue.Count; i++)
-			{
-				MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue();
-				threadInfo.callback(threadInfo.parameter);
+	void Update() {
+		if (mapDataThreadInfoQueue.Count > 0) {
+			for (int i = 0; i < mapDataThreadInfoQueue.Count; i++) {
+				MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue ();
+				threadInfo.callback (threadInfo.parameter);
 			}
 		}
 
-		if (meshDataThreadInfoQueue.Count > 0)
-		{
-			for (int i = 0; i < meshDataThreadInfoQueue.Count; i++)
-			{
-				MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue();
-				threadInfo.callback(threadInfo.parameter);
+		if (meshDataThreadInfoQueue.Count > 0) {
+			for (int i = 0; i < meshDataThreadInfoQueue.Count; i++) {
+				MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue ();
+				threadInfo.callback (threadInfo.parameter);
 			}
 		}
 	}
 
-	MapData GenerateMapData(Vector2 centre)
-	{
-		float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, centre + offset, normalizeMode);
+	MapData GenerateMapData(Vector2 centre) {
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize + 2, mapChunkSize + 2, seed, noiseScale, octaves, persistance, lacunarity, centre + offset, normalizeMode);
 
 		Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
-		for (int y = 0; y < mapChunkSize; y++)
-		{
-			for (int x = 0; x < mapChunkSize; x++)
-			{
-				if (useFalloff)
-				{
-					noiseMap[x, y] = Mathf.Clamp01(noiseMap[x, y] - falloffMap[x, y]);
+		for (int y = 0; y < mapChunkSize; y++) {
+			for (int x = 0; x < mapChunkSize; x++) {
+				if (useFalloff) {
+					noiseMap [x, y] = Mathf.Clamp01(noiseMap [x, y] - falloffMap [x, y]);
 				}
-				float currentHeight = noiseMap[x, y];
-				for (int i = 0; i < regions.Length; i++)
-				{
-					if (currentHeight >= regions[i].height)
-					{
-						colourMap[y * mapChunkSize + x] = regions[i].colour;
-					}
-					else
-					{
+				float currentHeight = noiseMap [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight >= regions [i].height) {
+						colourMap [y * mapChunkSize + x] = regions [i].colour;
+					} else {
 						break;
 					}
 				}
@@ -153,29 +124,25 @@ public class MapGenerator : MonoBehaviour
 		}
 
 
-		return new MapData(noiseMap, colourMap);
+		return new MapData (noiseMap, colourMap);
 	}
 
-	void OnValidate()
-	{
-		if (lacunarity < 1)
-		{
+	void OnValidate() {
+		if (lacunarity < 1) {
 			lacunarity = 1;
 		}
-		if (octaves < 0)
-		{
+		if (octaves < 0) {
 			octaves = 0;
 		}
 
-		falloffMap = FalloffGenerator.GenerateFalloffMap(mapChunkSize);
+		falloffMap = FalloffGenerator.GenerateFalloffMap (mapChunkSize);
 	}
 
-	struct MapThreadInfo<T>
-	{
+	struct MapThreadInfo<T> {
 		public readonly Action<T> callback;
 		public readonly T parameter;
 
-		public MapThreadInfo(Action<T> callback, T parameter)
+		public MapThreadInfo (Action<T> callback, T parameter)
 		{
 			this.callback = callback;
 			this.parameter = parameter;
@@ -186,19 +153,17 @@ public class MapGenerator : MonoBehaviour
 }
 
 [System.Serializable]
-public struct TerrainType
-{
+public struct TerrainType {
 	public string name;
 	public float height;
 	public Color colour;
 }
 
-public struct MapData
-{
+public struct MapData {
 	public readonly float[,] heightMap;
 	public readonly Color[] colourMap;
 
-	public MapData(float[,] heightMap, Color[] colourMap)
+	public MapData (float[,] heightMap, Color[] colourMap)
 	{
 		this.heightMap = heightMap;
 		this.colourMap = colourMap;
--- Assets/Scripts/Map Generation/Noise.cs
+++ Assets/Scripts/Map Generation/Noise.cs
@@ -1,34 +1,30 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
-public static class Noise
-{
+public static class Noise {
 
-	public enum NormalizeMode { Local, Global };
+	public enum NormalizeMode {Local, Global};
 
-	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset, NormalizeMode normalizeMode)
-	{
-		float[,] noiseMap = new float[mapWidth, mapHeight];
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset, NormalizeMode normalizeMode) {
+		float[,] noiseMap = new float[mapWidth,mapHeight];
 
-		System.Random prng = new System.Random(seed);
+		System.Random prng = new System.Random (seed);
 		Vector2[] octaveOffsets = new Vector2[octaves];
 
 		float maxPossibleHeight = 0;
 		float amplitude = 1;
 		float frequency = 1;
 
-		for (int i = 0; i < octaves; i++)
-		{
-			float offsetX = prng.Next(-100000, 100000) + offset.x;
-			float offsetY = prng.Next(-100000, 100000) - offset.y;
-			octaveOffsets[i] = new Vector2(offsetX, offsetY);
+		for (int i = 0; i < octaves; i++) {
+			float offsetX = prng.Next (-100000, 100000) + offset.x;
+			float offsetY = prng.Next (-100000, 100000) - offset.y;
+			octaveOffsets [i] = new Vector2 (offsetX, offsetY);
 
 			maxPossibleHeight += amplitude;
 			amplitude *= persistance;
 		}
 
-		if (scale <= 0)
-		{
+		if (scale <= 0) {
 			scale = 0.0001f;
 		}
 
@@ -39,51 +35,40 @@ public static class Noise
 		float halfHeight = mapHeight / 2f;
 
 
-		for (int y = 0; y < mapHeight; y++)
-		{
-			for (int x = 0; x < mapWidth; x++)
-			{
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
 
 				amplitude = 1;
 				frequency = 1;
 				float noiseHeight = 0;
 
-				for (int i = 0; i < octaves; i++)
-				{
-					float sampleX = (x - halfWidth + octaveOffsets[i].x) / scale * frequency;
-					float sampleY = (y - halfHeight + octaveOffsets[i].y) / scale * frequency;
+				for (int i = 0; i < octaves; i++) {
+					float sampleX = (x-halfWidth + octaveOffsets[i].x) / scale * frequency;
+					float sampleY = (y-halfHeight + octaveOffsets[i].y) / scale * frequency;
 
-					float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
 					noiseHeight += perlinValue * amplitude;
 
 					amplitude *= persistance;
 					frequency *= lacunarity;
 				}
 
-				if (noiseHeight > maxLocalNoiseHeight)
-				{
+				if (noiseHeight > maxLocalNoiseHeight) {
 					maxLocalNoiseHeight = noiseHeight;
-				}
-				else if (noiseHeight < minLocalNoiseHeight)
-				{
+				} else if (noiseHeight < minLocalNoiseHeight) {
 					minLocalNoiseHeight = noiseHeight;
 				}
-				noiseMap[x, y] = noiseHeight;
+				noiseMap [x, y] = noiseHeight;
 			}
 		}
 
-		for (int y = 0; y < mapHeight; y++)
-		{
-			for (int x = 0; x < mapWidth; x++)
-			{
-				if (normalizeMode == NormalizeMode.Local)
-				{
-					noiseMap[x, y] = Mathf.InverseLerp(minLocalNoiseHeight, maxLocalNoiseHeight, noiseMap[x, y]);
-				}
-				else
-				{
-					float normalizedHeight = (noiseMap[x, y] + 1) / (maxPossibleHeight / 0.9f);
-					noiseMap[x, y] = Mathf.Clamp(normalizedHeight, 0, int.MaxValue);
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				if (normalizeMode == NormalizeMode.Local) {
+					noiseMap [x, y] = Mathf.InverseLerp (minLocalNoiseHeight, maxLocalNoiseHeight, noiseMap [x, y]);
+				} else {
+					float normalizedHeight = (noiseMap [x, y] + 1) / (maxPossibleHeight/0.9f);
+					noiseMap [x, y] = Mathf.Clamp(normalizedHeight,0, int.MaxValue);
 				}
 			}
 		}
